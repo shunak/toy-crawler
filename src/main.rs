c0ef5af1fdf9a402b71f501cd94448d1c682972c
shunak <shuna966@gmail.com>
2022-08-31 12:18:44 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
+use select::document::Document;
+use select::predicate::Name;
+use url::{Url,ParseError};
+
 fn main() -> eyre::Result<()> {
 
-    let body = reqwest::blocking::get("https://www.rust-lang.org")?
-    .text()?;
+    let response = reqwest::blocking::get("https://www.rust-lang.org")?;
+    let base_url = response.url().clone();
+    let body = response.text()?;
+    let doc = Document::from(body.as_str());
 
-    println!("body = {:?}", body);
+    for href in doc.find(Name("a")).filter_map(|a| a.attr("href")){
+        // println!("{:?}", Url::parse(href));
+
+        match Url::parse(href) {
+            Ok(url) => { println!("{}", url); },
+            Err(ParseError::RelativeUrlWithoutBase) => {
+                let url = base_url.join(href)?;
+                println!("{}",url);
+            },
+            Err(e) => {println!("Error: {}",e)},
+        }
+
+
+    }
+
+    // println!("body = {:?}", body);
 
     Ok(())
-
 }
