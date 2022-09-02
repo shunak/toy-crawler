138fb881a332f68138340b6787c4dc43a57e3d42
shunak <shuna966@gmail.com>
2022-09-02 18:53:25 +0900

    wip

--- src/parse.rs
+++ src/parse.rs
@@ -10,7 +10,9 @@ pub fn main() -> eyre::Result<Vec<String>> {
     let body = response.text()?;
     let doc = Document::from(body.as_str());
     let mut json: Vec<String> = vec![];
-    let offset: u32 = 33;
+    let offset_from: u32 = 33;
+    let offset_to: u32 = 54;
+    let mut json_cnt1: String = "".to_string();
 
     // let url: &str = "";
     let mut idx: u32 = 0;
@@ -25,7 +27,7 @@ pub fn main() -> eyre::Result<Vec<String>> {
         // println!("{}",idx);
 
         // Display Article only that is latest.
-        if idx <= 33 || idx >= 54 {
+        if idx <= offset_from || idx >= offset_to {
              continue;
         }
 
@@ -39,15 +41,31 @@ pub fn main() -> eyre::Result<Vec<String>> {
 
                 let doc2 = Document::from(reqwest::blocking::get(url.clone())?.text()?.as_str());
 
-                let id = idx - offset;
+                let id = idx - offset_from;
+
                 for title in doc2.find(Name("h1")).filter_map(|b| b.first_child()){
 
-                        println!("ID:{} {:?} {}",id, title.html().replace("\u{3000}"," ").replace("&nbsp;",""), url.clone());
+                        // println!("ID:{} {:?} {}",id, title.html().replace("\u{3000}"," ").replace("&nbsp;",""), url.clone());
 
-                        // Construct JSON format.
-                        let json_cnt1 = String::from("id") + &id.to_string() + ":{article title:" + &title.html().replace("\u{3000}"," ").replace("&nbsp;","").to_string() + ",url:" + &url.clone().to_string() + "}";
+                       // Construct JSON format.
+                        if id == 1 {
 
-                        json.push(json_cnt1);
+                        json_cnt1 = String::from(r#""articles":["#) + "{\"id\":" + &id.to_string() + ",\"article title\":\"" +  &title.html().replace("\u{3000}"," ").replace("&nbsp;","")+ "\",\"url\":\"" + &url.clone().to_string() + "\"}";
+
+                        } else if 2 <= id  && id <= 19{
+
+                        json_cnt1 = String::from("{\"id\":")+ &id.to_string() + ",\"article title\":\"" +  &title.html().replace("\u{3000}"," ").replace("&nbsp;","")+ "\",\"url\":\"" + &url.clone().to_string() + "\"}";
+
+                        }else{
+
+                        json_cnt1 = String::from("{\"id\":")+ &id.to_string() + ",\"article title\":\"" +  &title.html().replace("\u{3000}"," ").replace("&nbsp;","")+ "\",\"url\":\"" + &url.clone().to_string() + "\"}]";
+
+                        }
+
+
+                       // let json_cnt1 = String::from("id")  + &id.to_string() + ":{\"article title\":\"" +  &title.html().replace("\u{3000}"," ").replace("&nbsp;","").replace("\\"," ").to_string() + "\",\"url\":\"" + &url.clone().to_string() + "\"}";
+
+                        json.push(json_cnt1.replace("\\"," "));
 
                 }
 
