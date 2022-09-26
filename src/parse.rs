d1fc8e97dcbd73750f8b210dd2df31e225d12a44
shunak <shuna966@gmail.com>
2022-09-26 17:36:13 +0900

    Modified: Skip range for necessary article.

--- src/parse.rs
+++ src/parse.rs
@@ -17,11 +17,16 @@ pub fn main() -> eyre::Result<Vec<String>> {
     let body = response.text()?;
     let doc = Document::from(body.as_str());
     let mut json: Vec<String> = vec![];
-    let offset_from: u32 = 33;
-    let offset_to: u32 = 54;
+    let possible_offset_from: Vec<u32> = vec![34,36]; // index 0 is in case the weather is typhoon etc.
+
+    let mut range_to = 21;
+    let mut current_pos_from = possible_offset_from[1];
+    let mut current_pos_to = current_pos_from + range_to;
+
+    // let offset_from: u32 = 32;
+    // let offset_to: u32 = 53;
     let mut content: String = "".to_string();
 
-    // let url: &str = "";
     let mut idx: u32 = 0;
     // println!("{:?}", doc);
     // for href in doc.find(Name("a")){
@@ -35,7 +40,7 @@ pub fn main() -> eyre::Result<Vec<String>> {
         // println!("{}",idx);
 
         // Display Article only that is latest.
-        if idx <= offset_from || idx >= offset_to {
+        if idx <= current_pos_from || idx >= current_pos_to {
              continue;
         }
 
@@ -49,10 +54,11 @@ pub fn main() -> eyre::Result<Vec<String>> {
 
                 let doc2 = Document::from(reqwest::blocking::get(url.clone())?.text()?.as_str());
 
-                let id = idx - offset_from;
+                let id = idx - current_pos_from;
 
                 for title in doc2.find(Name("h1")).filter_map(|b| b.first_child()){
 
+                    // println!("{}",idx);
                      // println!("ID:{} {:?} {}",id, title.html().replace("\u{3000}"," ").replace("&nbsp;",""), url.clone());
 
                      // Construct JSON format.
